77c7388ccafd627751f60236909596cf80aa6033
lanou3g <752162305@qq.com>
2015-10-20 09:48:06 +0800

    newslabel

---
+++ GourmetShare/GourmetShare/Source/CodeClass/View/NewsDetailTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  NewsDetailTableViewCell.h
+//  GourmetShare
+//
+//  Created by lanou3g on 15/10/19.
+//  Copyright © 2015年 jang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsDetailTableViewCell : UITableViewCell
+
+@property (nonatomic,strong)UILabel *newsLabel;
+
+@end
