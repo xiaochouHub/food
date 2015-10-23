9af22855a43d50aac24a70173ef1d62a20bab875
lanou3g <752162305@qq.com>
2015-10-23 15:04:23 +0800

    添加个人信息,关于我们界面

---
+++ GourmetShare/GourmetShare/Source/CodeClass/View/MySelfImageTableViewCell.m
@@ -0,0 +1,30 @@
+//
+//  MySelfImageTableViewCell.m
+//  GourmetShare
+//
+//  Created by lanou3g on 15/10/23.
+//  Copyright © 2015年 jang. All rights reserved.
+//
+
+#import "MySelfImageTableViewCell.h"
+
+@implementation MySelfImageTableViewCell
+
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if (self == [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        [self p_setup];
+    }
+    return self;
+}
+
+-(void)p_setup
+{
+    self.MyImage = [[UIImageView alloc]initWithFrame:CGRectMake(CGRectGetMaxX([[UIScreen mainScreen]bounds]) / 2 - 50, 50, 100, 100)];
+    self.MyImage.layer.cornerRadius = 50;
+    self.MyImage.layer.masksToBounds = YES;
+    self.MyImage.image = [UIImage imageNamed:@"3.jpg"];
+    [self.contentView addSubview:_MyImage];
+}
+
+@end
